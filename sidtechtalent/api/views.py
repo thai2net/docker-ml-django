1c1721d0e9d7a76ecc04a84f04f8ebdf49a6ea9b
Hiruma <yapadubpadoo@gmail.com>
2017-10-03 02:05:45 +0700

    add dockerfile, docker-compose, django base project

---
+++ sidtechtalent/api/views.py
@@ -0,0 +1,11 @@
+from rest_framework import viewsets
+
+from django.utils.encoding import force_text
+from django.http import JsonResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class Hello(APIView):
+    def get(self, request):
+        return Response({"hello": "world"})
+    
